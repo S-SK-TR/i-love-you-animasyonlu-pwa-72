a19eab3545fef77dc4fa4b2bc2d388f5dfc4ac57
S-SK-TR <serdarkul12@gmail.com>
2026-05-16 16:42:44 +0300

    ✨ Task pipeline tamamlandı (1 görev)

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,10 +4,16 @@ module.exports = {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: 'class',
   theme: {
     extend: {
       fontFamily: {
         sans: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        pink: {
+          500: '#ff6b6b',
+        },
       },
     },
   },
